3cbed702b3ebd09940baf90309730b46118998ac
mkondratek <mik.kondratek@gmail.com>
2022-09-03 22:12:25 +0200

    Try rgb->lms->xyb->dct conversion (failed)

--- commons.py
+++ commons.py
@@ -1,7 +1,51 @@
 import numpy as np
+import scipy
 import tensorflow as tf
 import torchjpeg.codec
 from progress.bar import Bar
+import cv2
+
+rgb2lms = np.array([
+    [17.8824, 43.5161, 4.1194],
+    [3.4557, 27.1554, 3.8671],
+    [0.0300, 0.1843, 1.4671]
+])
+
+lms2xyb = np.array([
+    [1, -1, 0],
+    [1, 1, 0],
+    [0, 0, 1]
+], dtype=np.float64)
+
+rgb2xyb = np.dot(lms2xyb, rgb2lms)
+
+
+def dct2(a):
+    return scipy.fftpack.dct(scipy.fftpack.dct(a, axis=0, norm='ortho'), axis=1, norm='ortho')
+
+
+def load_xyb(fname, *, transpose):
+    rgb = cv2.imread(fname)
+    h, w, _ = rgb.shape
+    xyb = np.zeros((h, w, 3))
+    for y in range(h):
+        for x in range(w):
+            xyb[y][x] = np.dot(rgb2xyb, rgb[y][x])
+
+    xyb = np.array([xyb[:, :, 0], xyb[:, :, 1], xyb[:, :, 2]])
+
+    for c in [0, 1, 2]:
+        for y in range(0, h, 8):
+            for x in range(0, w, 8):
+                xyb[c][y:(y + 8), x:(x + 8)] = dct2(xyb[c][y:(y + 8), x:(x + 8)])
+
+    for y in range(h):
+        for x in range(w):
+            print(f'bx={x} by={y}:')
+            for a in range(8):
+                for b in range(8):
+                    print(f'{xyb[y][x][b][a]}', end=' ')
+                print()
 
 
 def load_data(fname, *, transpose):
